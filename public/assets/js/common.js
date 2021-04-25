04621eb0f8b2168b50b9e6236a48fdca8e12b5c1
03DAICHI18 <dai.03.chi.18@gmail.com>
2021-04-25 13:41:12 +0900

    HTML記述

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -10,4 +10,15 @@ $(function () {
     }, speed, "swing");
     return false;
   });
+
+  // アコーディオン
+  $('.bl_accordion_innerMenu:not(:first-of-type)').css("display", "none");
+  $('.js_accordion_btn').on('click', function () {
+    $(this).children().addClass('is_open');
+    //クリックされていないel_accordion_iconのopenクラスを取る
+    $('.el_accordion_icon').not($(this).children()).removeClass('is_open');
+
+    $(this).parent().next().slideDown(300);
+    $('.bl_accordion_menu').not($(this).parent()).next().slideUp();
+  });
 });
---
+++ public/assets/js/swiper.js
@@ -0,0 +1,23 @@
+var mySwiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  slidesPerView: 3,
+  // 自動スライド
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  // ブレイクポイント
+  breakpoints: {
+    1025: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      centeredSlides: true,
+    },
+  },
+  // フリック操作
+  allowTouchMove: true,
+})
